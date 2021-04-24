fdf6a0e2c76a7102190dc6a39faaa575b4c80a77
lebashir <bashir.abihabib@gmail.com>
2021-04-24 10:08:22 -0500

    added some basic styling to the site

--- seeds/index.js
+++ seeds/index.js
@@ -21,9 +21,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i =0; i<50; i++) {
         const random1000 = Math.floor(Math.random() * 1000) +1;
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet nisi rem blanditiis quidem, nam suscipit quia quibusdam! Similique, officia nemo quaerat ullam doloribus illum, ex, nihil quam eveniet deleniti corporis.',
+            price
         })
         await camp.save();
     }
