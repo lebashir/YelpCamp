ff62cf5f959638c2cdf902533a93d001c5fb8325
lebashir <bashir.abihabib@gmail.com>
2021-04-26 13:50:47 -0500

    configured session and added flash partials

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -27,16 +27,20 @@ router.get('/new', (req, res) => {
 })
 
 router.post('/', validateCampground, catchAsync(async (req, res, next) => {
-       
         const campground = new Campground(req.body.campground);
         console.log(campground);
         await campground.save();
+        req.flash('success', 'Campground Created!');
         res.redirect(`/campgrounds/${campground._id}`);
 }))
 
 router.get('/:id', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id).populate('reviews');
-    res.render('campgrounds/show', {campground });
+    if(!campground) {
+        req.flash('error', 'Campground Not Found');
+        return res.redirect('/campgrounds');
+    }
+    res.render('campgrounds/show', {campground, });
 }))
 
 router.get('/:id/edit', catchAsync(async (req, res) => {
@@ -47,12 +51,14 @@ router.get('/:id/edit', catchAsync(async (req, res) => {
 router.put('/:id', validateCampground, catchAsync(async (req, res) => {
     const {id} = req.params;
     const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground}, {new: true})
+    req.flash('success', 'Campground Updated!')
     res.redirect(`/campgrounds/${campground._id}`, )
 }))
 
 router.delete('/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
+    req.flash('success', 'Campground Deleted Successfully')
     res.redirect('/campgrounds');
 }));
 
