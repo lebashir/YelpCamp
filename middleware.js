c35394af54a33f5fd50f250062f39689b5030a3c
lebashir <bashir.abihabib@gmail.com>
2021-04-29 17:40:06 -0500

    added some security features

--- middleware.js
+++ middleware.js
@@ -45,9 +45,9 @@ module.exports.validateCampground = (req, res, next) =>{
 }
 
 module.exports.validateReview = (req, res, next) => {
-    const {error} = reviewSchema.validate(req.body);
+    const { error } = reviewSchema.validate(req.body);
     if (error) {
-        const msg = error.details.map(el => el.message.join(','))
+        const msg = error.details.map(el => el.message).join(',')
         throw new ExpressError(msg, 400)
     } else {
         next();
